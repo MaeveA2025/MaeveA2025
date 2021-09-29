db71601bd2b063d659537b27b8dab515effb9de3
etan Memeignton <eja9979@g.rit.edu>
2021-09-29 19:26:43 -0400

    final roller

--- PythonProjects/die_roller.py
+++ PythonProjects/die_roller.py
@@ -1,6 +1,25 @@
 import random
-print("Hi! Welcome to die roll, pick 2 numbers!")
-first = int((input("First number: ")))
-second = int((input("Second number: ")))
-result = random.randint(first, second)
-print("The number was ", result, "!", sep="")
+def roller():
+    try:
+        # Ask for user input 
+        num = input("Hello welcome to die roller! Enter 2 numbers: ")
+        #split input into tokens
+        split_num = num.split()
+        # Result is a random number between the first [0th index] and second [1st index] numbers
+        # Lower to higher otherwise
+        result = random.randint(int(split_num[0]), int(split_num[1]))
+        # prints out the result of the randomizer
+        print("The number was ", result, "!", sep="")
+        # Calls roller again for more inputs
+        roller()
+    
+    # If there is an invalid number catch it and re run the function
+    except ValueError:
+        print("Invalid input")
+        roller()
+
+# Define main as our roller function and call it
+def main():
+    roller()
+
+main()
