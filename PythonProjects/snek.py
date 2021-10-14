5a5528bc1600b88cca635d150e0355925ec2848d
etan Memeignton <eja9979@g.rit.edu>
2021-10-14 18:59:32 -0400

    movement implemented

--- PythonProjects/snek.py
+++ PythonProjects/snek.py
@@ -1,25 +1,61 @@
 import pygame
 
 def game():
-    # initalizes the screen and the size of it
+    # initalizes the screent
     pygame.init()
-    dis = pygame.display.set_mode((400,300))
 
-    # Sets a caption on the window
-    pygame.display.set_caption("Snake game by Edureka")
-
-    # Initalizes color values using rgb values
-    blue = (0,0,255)
+    # Sets color values
+    white = (255,255,255)
+    black = (0,0,0)
     red = (255,0,0)
 
+    # Sets a caption on the window and size of window
+    dis = pygame.display.set_mode((800,600))
+    pygame.display.set_caption("Snake game by Edureka")
+
     game_over = False
+
+    # Initalize x and y values
+    x1 = 300
+    y1 = 300
+
+    x1_change = 0
+    y1_change = 0
+
+    #intalize clock
+    clock = pygame.time.Clock()
+
     while game_over != True:
         # If you quit the application the window will close
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 game_over = True
-        pygame.draw.rect(dis,blue,[200,150,10,10])
+            # If key is pressed then...
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -10
+                    y1_change = 0
+                if event.key == pygame.K_RIGHT:
+                    x1_change = 10
+                    y1_change = 0
+                if event.key == pygame.K_UP:
+                    x1_change = 0
+                    y1_change = -10
+                if event.key == pygame.K_DOWN:
+                    x1_change = 0
+                    y1_change = 10
+
+        # Changes x and y based of key presses
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(white)
+        # Creates player rectangle and then updates clock & display
+        pygame.draw.rect(dis, black, [x1, y1, 10, 10])
+
         pygame.display.update()
+
+        clock.tick(30)
+
     pygame.quit()
     quit()
 
